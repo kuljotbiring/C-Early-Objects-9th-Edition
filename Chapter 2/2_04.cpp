8eacdd6191f344a2634d56a619ab45f8bc8895a8
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2018-08-16 19:12:29 -0700

    Update 2_04.cpp

--- Chapter 2/2_04.cpp
+++ Chapter 2/2_04.cpp
@@ -3,15 +3,15 @@
 **
 ** Date: February 6, 2017
 **
-** Challenge: Write a program that computes the tax and tip on a 
-** restaurant bill for a patron with a $44.50 meal charge. The tax 
+** Challenge: Write a program that computes the tax and tip on a
+** restaurant bill for a patron with a $44.50 meal charge. The tax
 ** should be 6.75 percent of the meal cost. The tip should be 15
 ** percent of the total after adding the tax. Display the meal cost,
 ** tax amount, tip amount, and total bill on the screen.
 *********************************************************************/
 
 #include <iostream>
-using namespace std;
+
 
 int main()
 {
@@ -20,10 +20,10 @@ int main()
            yourTip = .15 * (yourTax + mealCharge),
            yourTotal = mealCharge + yourTax + yourTip;
 
-    cout << "Your meal cost is " << mealCharge << endl;
-    cout << "Your tax for the meal is " << yourTax << endl;
-    cout << "Your tip is " << yourTip << endl;
-    cout << "The total amount due is " << yourTotal << endl;
+    std::cout << "Your meal cost is " << mealCharge << std::endl;
+    std::cout << "Your tax for the meal is " << yourTax << std::endl;
+    std::cout << "Your tip is " << yourTip << std::endl;
+    std::cout << "The total amount due is " << yourTotal << std::endl;
 
     return 0;
 }
