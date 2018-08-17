f997777206ccb03ba43a1b901e361dc08dafc0d4
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2018-08-16 19:13:55 -0700

    Update 2_05.cpp

--- Chapter 2/2_05.cpp
+++ Chapter 2/2_05.cpp
@@ -9,16 +9,15 @@
 *********************************************************************/
 
 #include <iostream>
-using namespace std;
+
 
 int main()
 {
-    //Initialize and define variables
     double carTank = 16,
            carMiles = 312,
            milesPerGallon = carMiles / carTank;
-    
-    //Display results
-    cout << "The car you are driving gives " << milesPerGallon << " mpg\n";
+
+    std::cout << "The care you are driving gives " << milesPerGallon << " mpg\n";
+
     return 0;
 }
