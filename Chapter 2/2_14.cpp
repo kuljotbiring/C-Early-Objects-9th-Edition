d3ed61df0dfb6c4a2ff67bc978ce98ffaf22e387
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2018-08-16 19:34:06 -0700

    Update 2_14.cpp

--- Chapter 2/2_14.cpp
+++ Chapter 2/2_14.cpp
@@ -4,23 +4,22 @@
 ** Date: June 3, 2018
 **
 ** Challenge: The star player of a high school basketball team is 74
-** inches tall. Write a program to compute and display the height in 
+** inches tall. Write a program to compute and display the height in
 ** feet/inches form.
 *********************************************************************/
 
 #include <iostream>
-using namespace std;
+
 
 int main()
 {
-    //Initialize and define variables
     int playerHeight = 74,
             playerFeet = playerHeight / 12,
             playerInches = playerHeight % 12;
 
-    //Display results
-    cout << "The basketball players height is: " << playerFeet << "\' ";
-    cout << playerInches << "\"" << endl;
+
+    std::cout << "The basketball players height is: " << playerFeet << "\' ";
+    std::cout << playerInches << "\"" << std::endl;
 
     return 0;
 }
