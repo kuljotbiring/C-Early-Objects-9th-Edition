02d17e33000d91ee352d46033c90bd358d37e454
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2018-08-16 19:31:15 -0700

    Update 2_13.cpp

--- Chapter 2/2_13.cpp
+++ Chapter 2/2_13.cpp
@@ -3,23 +3,23 @@
 **
 ** Date: June 3, 2018
 **
-** Challenge: A particular employee earns $39,000 annually. Write a 
+** Challenge: A particular employee earns $39,000 annually. Write a
 ** program that determines and displays what the amount of his gross
-** pay will be for each pay period if he is paid twice a month (24 
+** pay will be for each pay period if he is paid twice a month (24
 ** checks per year) and if he is paid bi-weekly.
 *********************************************************************/
 
 #include <iostream>
-using namespace std;
+
 
 int main()
 {
-    //Initialize and define variables
     int salary = 39000,
         twiceMonthly = salary / 24,
         twiceWeekly = salary / 26;
 
-    //Display results
-    cout << "The employee would receive a bi-montly salary of " << twiceMonthly << " and a bi-weekly salary of "
-         << twiceWeekly << endl;
+    std::cout << "The employee would receive a bi-montly salary of $"
+    << twiceMonthly << " and a bi-weekly salary of $" << twiceWeekly << std::endl;
+
+    return 0;
 }
