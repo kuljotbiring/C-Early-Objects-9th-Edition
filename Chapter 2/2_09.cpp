52d3139e1296ad84a7b6dbfbfe110094893de97e
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2018-08-16 19:29:03 -0700

    Update 2_09.cpp

--- Chapter 2/2_09.cpp
+++ Chapter 2/2_09.cpp
@@ -1,26 +1,23 @@
 /*********************************************************************
-** Author: Kendal Droddy
+** Author: Kuljot Biring
 **
 ** Date: June 3, 2018
 **
-** Challenge: An electronics company makes circuit boards that cost
-** $14.95 apiece to produce. Write a program to determine how much 
+** An electronics company makes circuit boards that cost
+** $14.95 apiece to produce. Write a program to determine how much
 ** the company should sell them for if it wants to make a 35 percent
 ** profit. Display results on the screen.
 *********************************************************************/
 
 #include <iostream>
-using namespace std;
+
 
 int main()
 {
-    //Initialize and define variables
-    const double BASIS = 14.95;
-    
-    double
-           sale = (BASIS * .35) + BASIS;
+    double basis = 14.95,
+           sale = (basis * .35) + basis;
 
-    //Display results
-    cout << "The company should sell the unit for " << sale << endl;
+    std::cout << "The company should sell the unit for " << sale << std::endl;
+
     return 0;
 }
