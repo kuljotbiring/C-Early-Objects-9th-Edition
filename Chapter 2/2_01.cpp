410b7e50e6ff19c0d9ab8515f573335858127f25
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2018-08-16 19:07:31 -0700

    Update 2_01.cpp

--- Chapter 2/2_01.cpp
+++ Chapter 2/2_01.cpp
@@ -3,21 +3,20 @@
 **
 ** Date: June 02, 2018
 **
-** Challenge: Write a program that stores the integers 50 and 100 in 
-** variables and stores the sum of these two in a variable named 
+** Challenge: Write a program that stores the integers 50 and 100 in
+** variables and stores the sum of these two in a variable named
 ** total. Display the total on the screen.
 *********************************************************************/
 
 #include <iostream>
-using namespace std;
+
 
 int main()
 {
-    int fifty = 50,         // variables
-    hundred = 100, total;
-    
+    int fifty = 50, hundred = 100, total;
     total = fifty + hundred;
 
-    cout << "The total of this program is " << total << endl;
+    std::cout << "The total of this program is " << total << std::endl;
     return 0;
 }
+
