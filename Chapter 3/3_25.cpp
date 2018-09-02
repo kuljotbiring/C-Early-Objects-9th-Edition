ba00859f0ea599e794ddd9482dd9d9722b27900a
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2018-09-02 15:42:59 -0700

    Update 3_25.cpp

--- Chapter 3/3_25.cpp
+++ Chapter 3/3_25.cpp
@@ -1,26 +1,45 @@
-//
-// Created by Kuljot Singh Biring on 6/16/18.
-//
+/*********************************************************************
+** Author: Kuljot Biring
+**
+** Date: June 16, 2018
+**
+** Last month Joe purchased 100 shares of stock for $45.50
+** per share and paid his stock broker a commission that amounted to
+** 2 percent of the total amount he paid for the stock.
+** T wo months later Joe sold the stock for $47.92 per share and paid
+** his stock broker another commission that amounted to 2 percent of
+** the total amount he received for the stock.Write a program that
+** displays the following information:
+**    • The amount of money Joe paid for the stock.
+**    • The amount of commission Joe paid his broker when he bought
+**      the stock.
+**    • The amount that Joe sold the stock for.
+**    • The amount of commission Joe paid his broker when he sold the
+**      stock.
+**    • The amount of profit or loss that Joe had after selling his
+**      stock and paying both broker commissions.
+*********************************************************************/
+
 #include <iostream>
 #include <iomanip>
 
-using namespace std;
-
 int main()
 {
-
+    // define and initialize variables
     double stockPurchase = 45.50 * 100,
             stockSell = 47.92 * 100,
             purchaseCommission = stockPurchase * .02,
             sellCommission = stockSell * .02,
             profitLoss = (stockSell + sellCommission) - (stockPurchase + purchaseCommission);
 
+    // display formatted results
+    std::cout << std::fixed << std::setprecision(2) << std::endl;
+    std::cout << "Purchase amount:     $" << std::setw(6) << stockPurchase << std::endl;
+    std::cout << "Purchase commission: $" << std::setw(7) << purchaseCommission << std::endl;
+    std::cout << "Sell amount:         $" << std::setw(6) << stockSell << std::endl;
+    std::cout << "Sell commission:     $" << std::setw(7) << sellCommission << std::endl;
+    std::cout << "Profit/Loss:         $" << std::setw(7) << profitLoss << std::endl;
 
-    cout << fixed << setprecision(2) << endl;
-    cout << "Purchase amount:     $" << setw(6) << stockPurchase << endl;
-    cout << "Purchase commission: $" << setw(7) << purchaseCommission << endl;
-    cout << "Sell amount:         $" << setw(6) << stockSell << endl;
-    cout << "Sell commission:     $" << setw(7) << sellCommission << endl;
-    cout << "Profit/Loss:         $" << setw(7) << profitLoss << endl;
+    return 0;
 
 }
