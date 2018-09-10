916df265a1828f71c5f996c8efb63fc6d438cd5a
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2018-09-09 21:28:04 -0700

    Update 4_1.cpp

--- chapter 4/4_1.cpp
+++ chapter 4/4_1.cpp
@@ -1,8 +1,19 @@
+/*********************************************************************
+** Author: Kuljot Biring
+**
+** Date: June 19, 2018
+**
+** Write a program that asks the user to enter two numbers.
+** The program should use the conditional operator to determine which
+** number is the smaller and which is the larger.
+*********************************************************************/
+
 #include <iostream>
 
 int main()
 {
-    double num1, num2;
+    double num1{},
+           num2{};
 
     std::cout << "Please type the first number and press enter\n";
     std::cin >> num1;
