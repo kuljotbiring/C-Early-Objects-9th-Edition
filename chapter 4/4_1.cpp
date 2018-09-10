73b1de34a5af6668c25d41ada8ded74b3b8aa576
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2018-09-09 21:26:19 -0700

    Update 4_1.cpp

--- chapter 4/4_1.cpp
+++ chapter 4/4_1.cpp
@@ -1,1 +1,22 @@
-hi
+#include <iostream>
+
+int main()
+{
+    double num1, num2;
+
+    std::cout << "Please type the first number and press enter\n";
+    std::cin >> num1;
+    std::cout <<  "Please type the second number and press enter\n";
+    std::cin >> num2;
+
+    if(num1 > num2)
+    {
+        std::cout << num1 << " is larger than " << num2 << std::endl;
+    }
+    else
+    {
+        std::cout << num1 << " is smaller than " << num2 << std::endl;
+    }
+
+    return 0;
+}
