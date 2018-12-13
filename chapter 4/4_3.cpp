085a9d0eb560aba52e21c2636cd48924e78cb17c
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2018-12-12 20:59:13 -0800

    Update 4_3.cpp

--- chapter 4/4_3.cpp
+++ chapter 4/4_3.cpp
@@ -1,6 +1,17 @@
-//
-// Created by Kuljot Singh Biring on 6/19/18.
-//
+/*********************************************************************
+** Author: Kendal Singh Biring
+**
+** Date: June 19, 2018
+**
+** Challenge: The date June 10, 1960, is special because when we write 
+** it in the following format, the month times the day equals the year.
+**          * 6/10/60
+** Write a program that asks the user to enter a month (in numeric 
+** form), a day, and a two -digit year. The program should then 
+** determine whether the month times the day is equal to the year. If 
+** so, it should display a message saying the date is magic. 
+** Otherwise, it should display a message saying the date is not magic.
+*********************************************************************/
 #include <iostream>
 
 using namespace std;
