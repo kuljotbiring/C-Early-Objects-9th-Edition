c67ef49c20326069eac2c92280b4390b86598164
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2018-12-13 21:47:20 -0800

    Update 4.4.cpp

--- chapter 4/4.4.cpp
+++ chapter 4/4.4.cpp
@@ -1,6 +1,19 @@
-//
-// Created by Kuljot Singh Biring on 6/19/18.
-//
+/*********************************************************************
+** Author: Kuljot Biring
+**
+** Date: February 19, 2018
+**
+** Challenge: An online book club awards points to its customers based
+** on the number of books purchased each month as follows:
+**        Books Purchased    Points Earned
+**              0                   0
+**              1                   5 
+**              2                  15
+**              3                  30
+**          4 or more              50
+** Write a program that asks the user to enter the number of books
+** purchased this month and then displays the number of points awarded.
+*********************************************************************/
 #include <iostream>
 
 
