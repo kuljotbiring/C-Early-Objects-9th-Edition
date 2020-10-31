3ac1b2eeac2720cbf1d62b7b7e8171952470501e
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2020-10-30 21:37:01 -0700

    Update 5_01.cpp

--- chapter5/5_01.cpp
+++ chapter5/5_01.cpp
@@ -5,7 +5,7 @@
 **
 ** Write a program that uses a loop to display the
 ** characters for each ASCII code 32 through 127. Display 16
-** characters on each line with one space between characters.
+** characters on each line with one space between the characters.
 **********************************************************************/
 
 #include <iostream>
